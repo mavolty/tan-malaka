bc438f1253535cef9542f67cfc8a6eaaf79a493f
Saddam Husain <sadamyggakpernahp@gmail.com>
2021-11-27 23:25:34 +0700

    add locomotive scroll

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true,
+  lerp: 0.075,
+});
